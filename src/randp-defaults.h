7bf301df5a45cfc798610fcf8c36bd51e2117ad1
Steven Ward <planet36@users.noreply.github.com>
2024-11-03 21:20:04 -0500

    Add comment with NIST recommendation for reseed interval

--- src/randp-defaults.h
+++ src/randp-defaults.h
@@ -15,4 +15,12 @@
 
 #define DEFAULT_RANDP_NUM_BLOCKS 0x10
 
+/**
+# [NIST Special Publication 800-90A Revision 1](https://csrc.nist.gov/pubs/sp/800/90/a/r1/final)
+### 10.2.1 CTR_DRBG
+##### Table 3: Definitions for the CTR_DRBG
+|                                                                | AES-128 |
+|----------------------------------------------------------------|---------|
+| Maximum number of requests between reseeds (*reseed_interval*) |    2^48 |
+*/
 #define DEFAULT_RANDP_RESEED_COUNTDOWN_MIN (1 << 10)
