eedea3cf61b1ae1e499fd1231ef4c50e69671da3
Steven Ward <planet36@users.noreply.github.com>
2025-09-18 14:50:09 -0400

    Add mm256_make_odd_epu* functions

--- src/make_odd.h
+++ src/make_odd.h
@@ -43,6 +43,38 @@ mm_make_odd_epu64(const __m128i a)
     return _mm_or_si128(a, _mm_set1_epi64x(1));
 }
 
+#if defined(__AVX2__)
+
+/// Make the packed unsigned 8-bit integers odd
+static inline __m256i
+mm256_make_odd_epu8(const __m256i a)
+{
+    return _mm256_or_si256(a, _mm256_set1_epi8(1));
+}
+
+/// Make the packed unsigned 16-bit integers odd
+static inline __m256i
+mm256_make_odd_epu16(const __m256i a)
+{
+    return _mm256_or_si256(a, _mm256_set1_epi16(1));
+}
+
+/// Make the packed unsigned 32-bit integers odd
+static inline __m256i
+mm256_make_odd_epu32(const __m256i a)
+{
+    return _mm256_or_si256(a, _mm256_set1_epi32(1));
+}
+
+/// Make the packed unsigned 64-bit integers odd
+static inline __m256i
+mm256_make_odd_epu64(const __m256i a)
+{
+    return _mm256_or_si256(a, _mm256_set1_epi64x(1));
+}
+
+#endif
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
