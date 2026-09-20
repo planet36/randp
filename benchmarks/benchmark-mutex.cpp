49f29079cc92ae5662dccd4030d37b16f78985eb
Steven Ward <planet36@users.noreply.github.com>
2026-09-20 16:26:06 -0400

    Prevent implicit cast of size_t to double

--- benchmarks/benchmark-mutex.cpp
+++ benchmarks/benchmark-mutex.cpp
@@ -26,7 +26,7 @@ BM_rand_bytes(benchmark::State& BM_state, func_t& fn, const size_t buf_size)
 
     // Counters are summed across threads.  kAvgThreads makes this the per-thread rate.
     BM_state.counters["bytes_per_second"] =
-        benchmark::Counter(static_cast<double>(BM_state.iterations()) * buf_size,
+        benchmark::Counter(static_cast<double>(BM_state.iterations() * buf_size),
                            benchmark::Counter::kAvgThreadsRate, benchmark::Counter::kIs1024);
 }
 
